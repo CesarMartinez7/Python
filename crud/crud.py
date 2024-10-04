2e8e2b8d439135b73d0f0e57379b11165311e1de
CesarMartinez77 <cesarwamartinez@gmail.com>
2024-10-04 10:49:23 -0500

    kwargs use

--- crud/crud.py
+++ crud/crud.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 import mysql.connector
 from tkinter import messagebox
 import sys
+from pyfiglet import Figlet
 
 
 
@@ -13,17 +14,18 @@ class Interfaz(ctk.CTk):
         ctk.CTk.__init__(self)
         self.geometry("400x400")
         self.title("CREATE")
-        self.iconbitmap("./crud/Python/crud/image.ico")
+        self.iconbitmap("C:/Users/APRENDIZ SENA/Desktop/REPO/Python/crud/image.ico")
         self.resizable(False,True)
         self.tittle = ctk.CTkLabel(self,200,20,text="Bienvenido",font=("Times new roman",30)).pack_configure(pady=30)
         self.emailEntry = ctk.CTkEntry(self,placeholder_text="Email",width=200)
         self.emailEntry.pack(pady=20)
         self.contraseñaEntry = ctk.CTkEntry(self,placeholder_text="Contraseña",width=200,show=".")
         self.contraseñaEntry.pack()
-        self.boton = ctk.CTkButton(self,text="Enviar al DB",width=200,command=self.obtener_Datos)
+        self.boton = ctk.CTkButton(self,text="Enviar",width=200,command=self.obtener_Datos)
         self.boton.pack_configure(pady=20)
         self.buttonQUIT = ctk.CTkButton(master=self,text="Cerrar",fg_color="red",hover_color="#cc2424",command=self.Quit,width=200)
         self.buttonQUIT.pack_configure(pady=4,padx=30)
+        self.eliminarRegistro = ctk.CTkButton(self,text="Elimina registro")
     
     
     def Quit(self):
@@ -35,29 +37,37 @@ class Interfaz(ctk.CTk):
             self.emailClean = self.emailEntry.get()
             self.contraseñaClean = self.contraseñaEntry.get()
             print(self.emailClean,self.contraseñaClean)
-            self.GuardarDB(self.emailClean,self.contraseñaClean)
+            self.query(email=self.emailClean,password=self.contraseñaClean)
         else:
             self.error = messagebox.showerror(title="Error",message="Por favor rellene los campos correctamente")
     
-    def GuardarDB(self,email,contraseña):
+    
+    def query(self,**kwargs):         
         try:
             db = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="oyasumipunpun",
+            password="oyasumi",
             database="crud")
-
             mycursor = db.cursor()
             sql = "INSERT INTO PERSONA (email,contraseña) values (%s,%s)"
-            datos = (email,contraseña)
+            datos = kwargs.values()
+            datos = tuple(datos)
             mycursor.execute(sql,datos)
             db.commit()
-            print("Todo insertado")
         except Exception as e:
             messagebox.showinfo(message=f"Lo sentimos el error fue {e}")
-            
-
+        else:
+            print("Insertado con exito")
+            f = Figlet(font='slant')
+            print(f.renderText('Procesado con exito'))    
+        
     
+    
+
+    def delete(self):
+        pass
+                    
     
     
 inte = Interfaz()
---
+++ crud/prueba.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+# objeto = {'email': ' cesawamartinez', 'passwordd': '32143434'}
+
+
+# # for key in objeto.keys():
+# #     print(key)
+    
+# # for value in objeto.values():
+# #     print(value)
+    
+    
+
+
+# for llave,valor in objeto.keys(),objeto.values():
+#     breakpoint()
+#     print("llaves", llave)
+#     print("valor", valor)
+
+
+
+def function():
+    a = 20
+    b = 20
+    c = 30
+    return a,b,c
+
+
+usuario,_,_ = function()
+print(usuario)
+
+
+
+
+
